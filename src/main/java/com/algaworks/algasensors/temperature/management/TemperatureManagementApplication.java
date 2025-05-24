d8881d05a501755e9bcd26c055313d8484d46088
thadeubatista <thadeu.batista@gmail.com>
2025-05-24 17:00:37 -0300

    commit inicial

---
+++ src/main/java/com/algaworks/algasensors/temperature/management/TemperatureManagementApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algasensors/temperature/management/TemperatureManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
