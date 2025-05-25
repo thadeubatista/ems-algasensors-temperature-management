bae564f39f9d3271e45bc2fbe0cf7011a4bb7012
thadeubatista <thadeu.batista@gmail.com>
2025-05-25 09:55:21 -0300

    Finalizando implmentação de microserviços

--- src/main/java/com/algaworks/algasensors/temperature/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/temperature/management/api/controller/SensorController.java
@@ -8,6 +8,9 @@ import com.algaworks.algasensors.temperature.management.domain.model.SensorId;
 import com.algaworks.algasensors.temperature.management.domain.repository.SensorRepository;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,6 +21,52 @@ import org.springframework.web.server.ResponseStatusException;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    @DeleteMapping("{sensorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable TSID sensorId){
+        sensorRepository.deleteById(new SensorId(sensorId));
+    }
+
+    @PutMapping("{sensorId}")
+    public SensorOutput update(@PathVariable TSID sensorId, @RequestBody SensorInput input){
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+        sensor.setName(input.name());
+        sensor.setIp(input.ip());
+        sensor.setLocation(input.location());
+        sensor.setProtocol(input.protocol());
+        sensor.setModel(input.model());
+        sensor = sensorRepository.saveAndFlush(sensor);
+        return getSensorOutput(sensor);
+    }
+
+    @PutMapping("{sensorId}/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable(@PathVariable TSID sensorId){
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+        sensor.setEnabled(true);
+        sensorRepository.saveAndFlush(sensor);
+    }
+
+    @DeleteMapping("{sensorId}/enabled")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void disabled(@PathVariable TSID sensorId){
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+        sensor.setEnabled(false);
+        sensorRepository.saveAndFlush(sensor);
+    }
+
+
+
+    @GetMapping
+    public Page<SensorOutput> searsh(@PageableDefault Pageable pageable) {
+        //Page<Sensor> sensors = sensorRepository.findAll(pageable);
+        return sensorRepository.findAll(pageable)
+                .map(this::getSensorOutput);
+    }
 
     @GetMapping("{sensorId}")
     public SensorOutput getOne(@PathVariable TSID sensorId){
@@ -51,7 +100,7 @@ public class SensorController {
                 sensor.getLocation(),
                 sensor.getProtocol(),
                 sensor.getModel(),
-                false
+                sensor.getEnabled()
         );
     }
 
